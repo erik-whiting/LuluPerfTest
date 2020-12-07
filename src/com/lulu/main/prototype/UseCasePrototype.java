e438813b66f1e5082382794135aac05e4ec41ac2
erik-whiting <erik@erikwhiting.com>
2020-12-06 19:47:48 -0600

    Updates to UseCasePrototype

--- src/com/lulu/main/prototype/UseCasePrototype.java
+++ src/com/lulu/main/prototype/UseCasePrototype.java
@@ -14,11 +14,10 @@ public class UseCasePrototype {
         UseCases useCases = new UseCases(new ArrayList<>() {
             {
                 add(new UseCase("Explore Albums (python)", customerUcScript1, "python", 1));
-//                add(new UseCase("Explore Albums (ruby)", customerUcScript2, "ruby", 2));
+                add(new UseCase("Explore Albums (ruby)", customerUcScript2, "ruby", 2));
             }
         });
 
         useCases.start();
-        System.out.println("breakpoint");
     }
 }
